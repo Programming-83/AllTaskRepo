472fa8ee27cb266be520f736366e19a0761a620a
jaivik <jaivik@LAPTOP-O5BJ02K>
2024-09-17 21:25:29 +0530

    Task_1 complite

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,10 +8,20 @@ using System.Threading.Tasks;
 namespace Task_1
 {
     internal class Program
-    {
+    { // WAP to Input two number and Find Sum
         static void Main(string[] args)
         {
+            int a;
+            Console.WriteLine("Enter first");
+            a= Convert.ToInt32(Console.ReadLine());
+            int b;
+            Console.WriteLine("Enter second");
+            b= Convert.ToInt32(Console.ReadLine()); 
+            //Console.WriteLine("sum="+(a+b));
+            Console.WriteLine($"Sum = {a+b}");
+            Console.ReadLine();
+           
+           
         }
     }
 }
-//ha
