0f09d080f1b810b64765e3ce8f9bc19c05571e54
jaivik <jaivik@LAPTOP-O5BJ02K>
2024-09-19 14:35:37 +0530

    param paramter program

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -7,58 +7,32 @@ using System.Threading.Tasks;
 
 namespace Task_2
 {
-    class Employee
+    class operation
     {
-        public int Eid { get; set; }
-        public string name { get; set; }
-        public int age { get; set; }
-        public string city { get; set; }
-  
-        public static List<Employee> getEmployees()
+        public int calculate(params int[] a)
         {
-            List<Employee> empList = new List<Employee>();
-            Employee emp1 = new Employee();
-        //First Employee
-            emp1.Eid = 1; emp1.name = "JOJo"; emp1.age = 25; emp1.city = "Mirzapur";
-            empList.Add(emp1);
-            // Second Employee
-            Employee emp2 = new Employee();
-            emp2.Eid = 2; emp2.name = "AOAO"; emp2.age = 26; emp2.city = "MI";
-            empList.Add(emp2);
-            //Third Employee
-            Employee emp3 = new Employee();
-            emp3.Eid = 3; emp3.name = "UOUO"; emp3.age = 24; emp3.city = " Pahon";
-            empList.Add(emp3);
-            //Fourth Employee
-            Employee emp4= new Employee();
-            emp4.Eid = 4; emp4.name = "POPO"; emp4.age = 28; emp4.city = "Domanpur";
-            empList.Add(emp4);
-            //Fifth Employee
-            Employee emp5= new Employee();
-            emp5.Eid = 5; emp5.name = "SOSO"; emp5.age = 27; emp5.city = "GU";
-            empList.Add(emp5);
-        return empList;
+            int s = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+            }
+            return s;
         }
     }
-
     class Program
+    {
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                List<Employee> empList=  Employee.getEmployees();
-            var city_name = empList.Select(x => x.city);
-            foreach ( var city in city_name )
-            {
-                Console.WriteLine(city);
-               
-            }
+            operation op = new operation();
+            int res = op.calculate(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            int avg = res / 10;
+            Console.WriteLine("Sum =" + res);
+            Console.WriteLine("Average=" + avg);
             Console.ReadLine();
         }
-        
-        
-        }
-
     }
+
+}
 
 
 
